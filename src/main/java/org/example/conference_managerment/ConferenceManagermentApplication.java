9f8af20b0e55e4fce91b7f7601f59de2870eac40
lilequan <2852947476@qq.com>
2025-04-30 20:28:34 +0800

    上传项目

---
+++ src/main/java/org/example/conference_managerment/ConferenceManagermentApplication.java
@@ -0,0 +1,13 @@
+package org.example.conference_managerment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceManagermentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceManagermentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/conference_managerment/ConferenceManagermentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.conference_managerment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceManagermentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
